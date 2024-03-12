aff739d1ee4a4643e05a82b4338264a1bd20228b
Ani Kingsetha <115573337+kingsetha@users.noreply.github.com>
2024-03-12 21:30:50 +0530

    Update  by ANI in Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,8 @@
 public Calculator{
   //ani
+  System.out.println("This is ANI");
+  System.out.println("Hoping for the better day");
+  System.out.println("Hi dude");
   //aravind
   //velan
   //vicky
